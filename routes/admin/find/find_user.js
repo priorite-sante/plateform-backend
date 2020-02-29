89d9f3a1e84c1b8653f230f1dae2cd19698dc024
luc-dotcom <lucm.dotcom@gmail.com>
2020-02-29 15:25:44 +0200

    new admin find user

---
+++ routes/admin/find/find_user.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const db = require('../../../DB/db')
+
+const bodyParser = require('body-parser')
+
+
+const router = express.Router()
+
+router.use(bodyParser.json())
+
+
+///FIND USER BY ID
+///
+///
+var findUserById = router.post('/admin_find_user_by_id', async (req, res)=>{
+
+    var id = req.body.id.toString()
+
+    var objectId = db.getPrimaryKey(id)
+
+    
+    ///SEARCH USER WITH THE ID PROVIDED BY QRCODE
+    ///
+    ///
+    await db.getDB().collection('users').find({_id: objectId}).toArray((err, user)=>{
+        if(err) {
+          console.error(err)
+          res.sendStatus(500);
+        }
+        
+        else {
+            res.json(user)
+        }
+       
+
+ })
+
+
+})
+
+///FIND USER BY NAME 
+///
+///
+
+
+var findUserByName = router.post('/admin_find_user_by_name', async (req, res)=>{
+
+  var name = req.body.name.toString()
+
+
+  
+  await db.getDB().collection('users').find({ name: name}).toArray((err, user)=>{
+      if(err) {
+        console.error(err)
+        res.sendStatus(500);
+      }
+      
+      else {
+          res.json(user)
+      }
+     
+
+})
+
+
+})
+
+
+
+
+module.exports =[findUserById, findUserByName]
