ac7f046d2d9f223ae9c9d408b2372c32ccdd0662
luc-dotcom <lucm.dotcom@gmail.com>
2020-05-20 08:40:39 +0200

    add login

---
+++ routes/users/login.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const db = require('../../../DB/db')
+
+const bodyParser = require('body-parser')
+
+
+const router = express.Router()
+
+router.use(bodyParser.json())
+
+
+///FIND USER BY ID
+///
+///
+var findUserById = router.post('/user_login', async (req, res)=>{
+
+    var id = req.body.id.toString()
+
+    var objectId = db.getPrimaryKey(id)
+
+    
+    ///SEARCH USER WITH THE ID PROVIDED BY QRCODE
+    ///
+    ///
+    await db.getDB().collection('users').find({_id: objectId}).toArray((err, user)=>{
+        if(err) {
+          console.error(err)
+          res.sendStatus(500);
+        }
+        
+        else {
+            res.json(user)
+            res.status(200)
+        }
+       
+
+ })
+
+
+})
+
+///FIND USER BY NAME 
+///
+///
+
+
+
