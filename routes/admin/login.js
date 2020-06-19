7896e950e9eb13a7e04673a5285bd0e7b24c649d
luc-dotcom <lucm.dotcom@gmail.com>
2020-06-19 22:36:11 +0200

    .

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
-const db = require('../../db/db');
+const db = require('../../database/db');
 const router = express.Router();
 
 
