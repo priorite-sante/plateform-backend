3bc90bfbc069d65c8b80a2e4aa9eede0f1dd941b
luc-dotcom <lucm.dotcom@gmail.com>
2020-07-20 20:42:03 +0200

    .

--- database/db.js
+++ database/db.js
@@ -4,8 +4,8 @@ const ObjectID = require("mongodb").ObjectID;
 // name of our database
 const dbname = "sante";
 // location of where our mongoDB database is located
-const url= "mongodb://localhost:27017/";
-const urlx = "mongodb+srv://luc:wC4l2wF1wD5V6R7k@cluster0-vbtlh.mongodb.net/test?retryWrites=true&w=majority"
+const urlc= "mongodb://localhost:27017/";
+const url = "mongodb+srv://luc:wC4l2wF1wD5V6R7k@cluster0-vbtlh.mongodb.net/test?retryWrites=true&w=majority"
 // Options for mongoDB
 const mongoOptions = {useNewUrlParser : true , useUnifiedTopology: true };
 
--- routes/admin/chart/user_stats.js
+++ routes/admin/chart/user_stats.js
@@ -19,7 +19,7 @@ var user = router.get('/admin/stats/get/user/', (req, res) => {
           res.sendStatus(500);
         }else {
           res.json({
-           "users": results.toString()
+           "user_number": results.toString()
           })
         }
       }
@@ -31,7 +31,7 @@ var user = router.get('/admin/stats/get/user/', (req, res) => {
           res.sendStatus(500);
         }else {
           res.json({
-           "childreen": results.toString()
+           "child_number": results.toString()
           })
         }
       }
@@ -43,11 +43,58 @@ var user = router.get('/admin/stats/get/user/', (req, res) => {
           res.sendStatus(500);
         }else {
           res.json({
-           "users": results.toString()
+           "today_registrer": results.toString()
           })
         }
       });
+    break;
+    case "today_hospital": db.getDB().collection('users').countDocuments(
+      {userHospital: todayDate}, (err, results) => {
+        if(err){
+          res.sendStatus(500);
+        }else {
+          res.json({
+           "today_hoapital": results.toString()
+          })
+        }
+      });
+    break;
+    case "total_hospital": db.getDB().collection('users').countDocuments(
+      {userHospital: {}}, (err, results) => {
+        if(err){
+          res.sendStatus(500);
+        }else {
+          res.json({
+           "total_hospital": results.toString()
+          })
+        }
+      });
+
+    case "child_h":db.getDB().collection('users').countDocuments(
+      {children: {type: 1}}, (err, results) => {
+        if(err){
+          res.sendStatus(500);
+        }else {
+          res.json({
+           "child_h": results.toString()
+          })
+        }
+      });
+    
+    break;
+    case "child_f":db.getDB().collection('users').countDocuments(
+      {children: {type: 2}}, (err, results) => {
+        if(err){
+          res.sendStatus(500);
+        }else {
+          res.json({
+           "child_f": results.toString()
+          })
+        }
+      });
+
     default: res.sendStatus(404);
+    
   }
  
 
